a64e6a83ff633630cbddec06626c6120191bd0d8
Sol Eugenia Perez Sanchez <Solperezsanchez03@gmail.com>
2024-03-11 19:10:30 -0300

    .

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const http = require('http');
+const socketIo = require('socket.io');
+const path = require('path');
+
 const productsRouter = require('./routes/products.js');
 const cartsRouter = require('./routes/carts.js');
 
 const app = express();
-const port = 8080;
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Configurar Handlebars como motor de vistas
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Configurar directorio de vistas
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware para manejar datos en formato JSON
 app.use(express.json());
@@ -14,6 +27,9 @@ app.use('/api/products', productsRouter);
 // Rutas para carritos
 app.use('/api/carts', cartsRouter);
 
-app.listen(port, () => {
-  console.log(`Servidor iniciado en http://localhost:${port}`);
+// Iniciar el servidor
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+    console.log(`Servidor iniciado en http://localhost:${PORT}`);
 });
+
